ed4c56c708a9a8c9c830ae939dbebfc2b3de885f
Ricardo Casía <ricardocasia.dev@gmail.com>
2022-07-08 19:25:16 +0200

    add bootstrap plugin to flask app

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
 
 todos = ["Hacer la compra", "Pintar la casa", "Entregar vídeo al productor"]
 
