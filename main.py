124ec4c2b93768f60eb13755994ff5b5da5e8585
Ricardo Casía <ricardocasia.dev@gmail.com>
2022-07-08 17:55:49 +0200

    rendered a list

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, make_response, redirect
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+todos = ["TODO1", "TODO2", "TODO3"]
 
 @app.route('/')
 def index():
@@ -15,5 +16,9 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = request.cookies.get('user_ip')
+    context = {
+                'user_ip': user_ip,
+                'todos': todos
+            }
 
-    return "Hello World Rico, tu IP es {}".format(user_ip)
+    return render_template('hello.html', **context)
