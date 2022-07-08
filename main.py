e4a3e39823fd4eec56ffe7618d8d489810fba81f
Ricardo Casía <ricardocasia.dev@gmail.com>
2022-07-08 20:39:30 +0200

    add tests

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
+import unittest
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -11,10 +13,18 @@ app.config['SECRET_KEY'] = 'SUPER SECRET KEY'
 
 todos = ["Hacer la compra", "Pintar la casa", "Entregar vídeo al productor"]
 
+
 class LoginForm(FlaskForm):
     username = StringField('Nombre de ususario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
+
 
 @app.errorhandler(404)
 def not_found(error):
@@ -25,7 +35,7 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr
 
-    response = make_response(redirect('/hello'))
+    response = make_response(redirect(url_for('hello')))
     session['user_ip'] = user_ip
 
     return response
